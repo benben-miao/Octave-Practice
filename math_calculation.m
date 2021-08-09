27ebdfc3d731971a19054aa2fdd54eae331ba918
benben-miao <benben.miao@outlook.com>
2021-08-09 22:40:11 +0800

    First init

---
+++ math_calculation.m
@@ -0,0 +1,13 @@
+#1. EN: Octave can easily be used for basic numerical calculations. Octave knows about arithmetic operations (+,-,*,/), exponentiation (^), natural logarithms/exponents (log, exp), and the trigonometric functions (sin, cos, …). Moreover, Octave calculations work on real or imaginary numbers (i,j). In addition, some mathematical constants such as the base of the natural logarithm (e) and the ratio of a circle’s circumference to its diameter (pi) are pre-defined. 
+#1. CN: Octave 可以很容易地用于基本的数值计算。 Octave 了解算术运算 (+,-,*,/)、幂运算 (^)、自然对数/指数 (log, exp) 和三角函数 (sin, cos, ...)。 Octave 计算适用于实数或虚数 (i,j)。 此外，还预先定义了一些数学常数，例如自然对数的底数 (e) 和圆的周长与直径的比值 (pi)。
+
+exp(i*pi)
+# ans = -1.0000e+00 + 1.2246e-16i
+
+e
+# ans = 2.7183
+
+pi
+# ans = 3.1416
+
+
